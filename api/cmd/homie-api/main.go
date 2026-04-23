746f984c55581fef00544cfc4b065c2c1f53161d
achex <chekhonin.artem@gmail.com>
2026-04-23 20:14:54 +0300

    сделал миграции и Go модуль

---
+++ api/cmd/homie-api/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/pressly/goose/v3"
+)
+
+func main() {
+	dbUser := os.Getenv("POSTGRES_USER")
+	dbPassword := os.Getenv("POSTGRES_PASSWORD")
+	dbHost := os.Getenv("POSTGRES_HOST")
+	dbPort := os.Getenv("POSTGRES_PORT")
+	dbName := os.Getenv("POSTGRES_DB")
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", dbUser, dbPassword, dbHost, dbPort, dbName)
+
+	mustUpMigrations(connStr)
+}
+
+func mustUpMigrations(connStr string) {
+	db, err := sqlx.Connect("pgx", connStr)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = goose.SetDialect("postgres")
+	if err != nil {
+		panic(err)
+	}
+
+	const migrationsDir = "db/migrations"
+	err = goose.Up(db.DB, migrationsDir)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ api/db/migrations/20260423165739_new_table_tg_offer.sql
@@ -0,0 +1,16 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS tg_house_offer (
+    id BIGSERIAL PRIMARY KEY NOT NULL,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    price DECIMAL (12, 2) NOT NULL,
+    type TEXT NOT NULL CHECK (type IN ('RENT', 'BUY')),
+    owner_id BIGINT NOT NULL REFERENCES tg_user (id)
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS tg_house_offer;
+-- +goose StatementEnd
