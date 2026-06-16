e13e65a1c46f844b08c821f8f87869daaa4a1321
achex <chekhonin.artem@gmail.com>
2026-06-16 14:47:25 +0300

    пофиксил отмену жалоб

--- bot/app/handlers/search_offers.py
+++ bot/app/handlers/search_offers.py
@@ -40,10 +40,7 @@ async def search_start(msg: Message, state: FSMContext):
 
     today_likes = await get_today_likes(msg.from_user.id)
     if not today_likes:
-        await msg.answer(
-            "Произошла непредвиденная ошибка на сервере. Попробуйте позже или сообщите в техподдержку: @homie_bot_support",
-            reply_markup=back_to_main_menu_keyboard
-        )
+        await show_server_error(msg)
         return
 
     await state.update_data(today_likes_count=today_likes.likes_count)
@@ -74,24 +71,32 @@ async def select_city(msg: Message, state: FSMContext):
     await send_mag(msg)
     await show_next_offer(msg, state)
 
-# TODO: Добавить параметр offer_id чтобы можно было явно указать, какое объявление показать следующим
-async def show_next_offer(msg: Message, state: FSMContext):
+async def show_next_offer(msg: Message, state: FSMContext, offer_id: int = 0, relevance: int = 0):
+    if offer_id != 0 and relevance == 0:
+        raise Exception("relevance == 0 when offer_id != 0")
+
     data = await state.get_data()
 
     flags = UserFlags(**data["user"]["flags"]) if "flags" in data.get("user", {}) else None
 
     city = data["city"]
-        
-    offer = await get_rand_offer(msg.from_user.id, city, flags)
-    if offer is None:
-        await msg.answer(
-            "Вы просмотрели все доступные на сегодня предложения. Возвращайтесь позже!",
-            reply_markup=back_to_main_menu_keyboard
-        )
-        return
     
-    relevance = offer.relevance_percent
-    offer = offer.offer
+    if offer_id != 0:
+        offer = await get_offer_by_id(offer_id)
+        if offer is None:
+            await show_server_error(msg)
+            return
+    else:
+        offer = await get_rand_offer(msg.from_user.id, city, flags)
+        if offer is None:
+            await msg.answer(
+                "Вы просмотрели все доступные на сегодня предложения. Возвращайтесь позже!",
+                reply_markup=back_to_main_menu_keyboard
+            )
+            return
+        
+        relevance = offer.relevance_percent
+        offer = offer.offer
 
     await state.update_data(offer_id=offer.id)
     await state.update_data(offer_relevance=relevance)
@@ -159,10 +164,13 @@ async def handle_report(msg: Message, state: FSMContext):
 
 @router.message(SearchOffers.report_reason, F.text == "Отмена")
 async def cancel_report(msg: Message, state: FSMContext):
+    # Показываем то же объявление, на которое юзер хотел пожаловаться
     data = await state.get_data()
     offer_id = int(data["offer_id"])
+    relevance = int(data["offer_relevance"])
+
     await send_mag(msg)
-    await show_next_offer(msg, state, offer_id)
+    await show_next_offer(msg, state, offer_id, relevance)
 
 @router.message(SearchOffers.report_reason)
 async def report_reason(msg: Message, state: FSMContext):
@@ -195,3 +203,9 @@ async def send_mag(msg: Message):
     )
 
     await msg.answer("🔎", reply_markup=kb)
+
+async def show_server_error(msg: Message):
+    await msg.answer(
+        "Произошла непредвиденная ошибка на сервере. Попробуйте позже или сообщите в техподдержку: @homie_bot_support",
+        reply_markup=back_to_main_menu_keyboard
+    )
